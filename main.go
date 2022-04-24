482db3ea4c2a6c012c1730539a0ed02f5983c989
alvaldes <angelluis2605@gmail.com>
2022-04-24 17:49:13 -0400

    Created a simple menu

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
-pakage main
+package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	fmt.Println("Hello World")
+func main() {
+	option := 0
+	fmt.Println("Hola Mundo, ¿Qué deseas hacer?")
+	fmt.Println("> 1. Imprmir Hola Mundo")
+	fmt.Println("> 2. Imprmir Hola Mundo")
+	fmt.Println("> 3. Imprmir Hola Mundo")
+	fmt.Scanf("%d", &option)
+	switch option {
+	case 1:
+		fmt.Println("Hola Mundo")
+	case 2:
+		fmt.Println("Hola Mundo")
+	case 3:
+		fmt.Println("Hola Mundo")
+	default:
+		fmt.Println("Opcion no valida")
+	}
 }
